fa0b4272c2d7a0ece1f6a04a804b8b7238733e90
박경민 <kyungparkmin@naver.com>
2023-04-03 10:20:29 +0900

    feat: remove application.properties

--- src/main/java/com/example/blogproject/controller/FileController.java
+++ src/main/java/com/example/blogproject/controller/FileController.java
@@ -1,8 +1,17 @@
 package com.example.blogproject.controller;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,17 +19,23 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.UUID;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class FileController {
     @Value("${file.dir}")
     private String fileDir;
 
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+    private final AmazonS3 s3client;
+
+
     @PostMapping("/image/upload")
     @ResponseBody
-    public ModelAndView image(MultipartHttpServletRequest request) throws Exception {
+    public ModelAndView image(MultipartHttpServletRequest request, @RequestPart(value="upload", required = false) MultipartFile image) throws Exception {
         ModelAndView mv = new ModelAndView("jsonView");
 
         MultipartFile uploadFile = request.getFile("upload");
@@ -47,6 +62,30 @@ public class FileController {
 
         mv.addObject("uploaded", true);
         mv.addObject("url", savePath);
+
+        return mv;
+    }
+
+
+
+
+    @PostMapping("/upload")
+    @ResponseBody
+    public ModelAndView upload(MultipartHttpServletRequest  request) throws IOException {
+        ModelAndView mv = new ModelAndView("jsonView");
+
+        MultipartFile file = request.getFile("upload");
+        String filename = file.getOriginalFilename();
+        String extension = filename.substring(filename.lastIndexOf(".") + 1);
+        String key = UUID.randomUUID().toString() + "." + extension;
+
+        s3client.putObject(bucketName, key, file.getInputStream(), null);
+
+        String url = s3client.getUrl(bucketName, key).toString();
+
+
+        mv.addObject("uploaded", true);
+        mv.addObject("url", url);
 
         return mv;
     }
